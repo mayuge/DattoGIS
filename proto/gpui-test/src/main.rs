82721d837cf85da1da6fc3ec6391018a760f0577
Keita Ohmuro <76515061+mayuge@users.noreply.github.com>
2026-05-21 21:24:31 +0900

    feat:コンポーネントテスト

---
+++ proto/gpui-test/src/main.rs
@@ -0,0 +1,62 @@
+use gpui::*;
+use gpui_component::{button::*, *};
+
+pub struct HelloWorld;
+
+impl Render for HelloWorld {
+    fn render(
+        &mut self,
+        _: &mut Window,
+        _: &mut Context<Self>,
+    ) -> impl IntoElement {
+        div()
+            .v_flex()
+            .gap_2()
+            .size_full()
+            .items_center()
+            .justify_center()
+            .child("Hello, World!")
+            .child(
+                Button::new("ok")
+                    .primary()
+                    .label("Let's Go!")
+                    .on_click(|_,_,_| {
+                        println!("Clicked!")
+                    })
+            )
+    }
+}
+
+fn main() {
+    let app =
+        gpui_platform::application()
+        .with_assets(
+            gpui_component_assets::Assets
+        );
+
+    app.run(move |cx| {
+
+        // 超重要
+        gpui_component::init(cx);
+
+        cx.spawn(async move |cx| {
+            cx.open_window(
+                WindowOptions::default(),
+                |window,cx| {
+                    let view=
+                        cx.new(|_| HelloWorld);
+
+                    cx.new(|cx| {
+                        Root::new(
+                            view,
+                            window,
+                            cx
+                        )
+                    })
+                }
+            )
+            .expect("Failed");
+        })
+        .detach();
+    });
+}
